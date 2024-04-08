010c8d630817c564ac7647fe8390290d0970b3f8
VaibhavVilas <vaibhav.karale@gslab.com>
2024-04-08 14:56:24 +0530

    made changes in common-disease usecase

--- Disease-Backend/diseases.py
+++ Disease-Backend/diseases.py
@@ -1,24 +1,24 @@
 data = [
     {"diseaseName": "Asthma"},
     {"diseaseName": "Stroke"},
-    {"diseaseName": "Osteoporosis"},
+    # {"diseaseName": "Osteoporosis"},
     {"diseaseName": "Hypertension"},
     {"diseaseName": "Migraine"},
     {"diseaseName": "Diabetes"},
     {"diseaseName": "Influenza"},
     {"diseaseName": "Pneumonia"},
-    {"diseaseName": "Bronchitis"},
-    {"diseaseName": "Hyperthyroidism"},
-    {"diseaseName": "Hypothyroidism"},
-    {"diseaseName": "Rheumatoid Arthritis"},
-    {"diseaseName": "Osteoarthritis"},
+    # {"diseaseName": "Bronchitis"},
+    # {"diseaseName": "Hyperthyroidism"},
+    # {"diseaseName": "Hypothyroidism"},
+    # {"diseaseName": "Rheumatoid Arthritis"},
+    # {"diseaseName": "Osteoarthritis"},
     {"diseaseName": "Depression"},
     {"diseaseName": "Common Cold"},
-    {"diseaseName": "Eczema"},
+    # {"diseaseName": "Eczema"},
     {"diseaseName": "Liver Cancer"},
-    {"diseaseName": "Allergic Rhinitis"},
+    # {"diseaseName": "Allergic Rhinitis"},
     {"diseaseName": "Kidney Cancer"},
-    {"diseaseName": "Gastroenteritis"}
+    # {"diseaseName": "Gastroenteritis"}
 ]
 
 
